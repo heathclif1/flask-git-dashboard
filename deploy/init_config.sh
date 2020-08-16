dce7584c50187d265cbc4078f5fdfa5e362bf43b
heathclif1 <69751301+heathclif1@users.noreply.github.com>
2020-08-16 13:28:00 +0100

    Update init_config.sh

--- deploy/init_config.sh
+++ deploy/init_config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -vvvv ./init_config.yml --private-key=/home/matt/do_deploy -u root -i ./hosts
+ansible-playbook -vvvv ./init_config.yml --private-key=/home/synaps/do_deploy -u root -i ./hosts
